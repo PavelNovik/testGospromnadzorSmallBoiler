f0459259b46efbdf219164f3c229c675fff4f341
PavelNovik <brn2befree@gmail.com>
2022-06-06 10:32:47 +0300

    synchro main

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 'use strict';
+
 // Importing const
 import { kotelnQuest } from './koteln.js';
 import { gazQuest } from './gaz.js';
 
 // add music
+const audioMusic = document.querySelector('audio');
+audioMusic.volume = 0.03;
+const musicStop = document.querySelector('.fa-volume-mute');
+const musicPlay = document.querySelector('.fa-volume-up');
+musicStop.addEventListener('click', function () {
+  audioMusic.muted = true;
+});
+musicPlay.addEventListener('click', function () {
+  audioMusic.muted = false;
+});
 
-// const audioMusic = document.querySelector('audio');
-// audioMusic.volume = 0.03;
-// const musicStop = document.querySelector('.fa-volume-mute');
-// const musicPlay = document.querySelector('.fa-volume-up');
-// musicStop.addEventListener('click', function() {
-//   audioMusic.muted = true
-// })
-// musicPlay.addEventListener('click', function () {
-// audioMusic.muted = false;
-// })
-
-// console.log(audioMusic.volume);
-
-// console.log(kotelnQuest);
 // Seting the elements of web
 // Text menu in the Header
 const howManyQuestions = document.querySelector('.allQuest');
@@ -36,15 +33,12 @@ const checkAnsw = document.querySelector('.quest__check');
 // Question Block
 const numsField = document.querySelector('.quest__nums');
 const currentQuestion = document.querySelector('.quest__text_block');
-const answers = document.querySelectorAll('.answs__text');
 const answerOne = document.querySelector('.answ__one_text');
 const answerTwo = document.querySelector('.answ__two_text');
 const answerThree = document.querySelector('.answ__three_text');
+
 // const allSymbls = document.querySelectorAll('.symb');
 const allLabels = document.querySelectorAll('label');
-const answOneSymb = document.querySelector('.symb__one');
-const answTwoSymb = document.querySelector('.symb__two');
-const answThreeSymb = document.querySelector('.symb__three');
 const answerTextBlock = document.querySelector('.answer__text');
 
 const inputs = document.querySelectorAll('input');
@@ -53,28 +47,20 @@ const comentRule = document.querySelector('.rule__link');
 const trueAnswer = document.querySelector('.true__answer');
 
 // Form
-
 const answerForm = document.querySelector('form');
 
 // All buttons
-
 const checkBtn = document.querySelector('.btn__check');
 const prewiousBtn = document.querySelector('.btn__previous_quest');
 const nextBtn = document.querySelector('.btn__next_quest');
 
-// Numbers of answers
-const numberOfAnswer = ['One', 'Two', 'Three'];
 // Form
 // const answerCallBack = document.querySelector('.answer__list');
-
-// console.log(checkBtn);
-// console.log(document.forms.answers[1]);
 let numberOfTrueAnswer = 0;
 let numberOfFalseAnswer = 0;
 let i = 0;
 
 // Print Nums block
-
 function printNums() {
   for (let i = 0; i < kotelnQuest.length; i++) {
     numsField.insertAdjacentHTML(
@@ -89,7 +75,6 @@ function printNums() {
 const allNumDivs = printNums();
 
 // newLook function
-
 function newLookPage() {
   for (let i = 0; i < kotelnQuest.length; i++) {
     if (kotelnQuest[i].answerUser) {
@@ -104,8 +89,6 @@ function newLookPage() {
   }
 }
 
-// В функцию ниже необходимо внести изменения для отрисовки разагданной страницы
-
 function startTest(questions, i) {
   infoField.textContent = 'Подготовка к проверке знаний: ';
   howManyQuestions.textContent = questions.length;
@@ -116,15 +99,19 @@ function startTest(questions, i) {
   questNum.textContent = i + 1;
   checkAnsw.style.opacity = 0;
   currentQuestion.textContent = questions[i].questText;
-  // let ind = randomiser();
-  // for (let n = 0; n <= 2; n++) {
-  //   answers[n].textContent = questions[i].answers[ind[n]];
-  //   inputs[n].value = ind[n] + 1;
-  // }
+  // имплементация рандомной раздачи ответов на вопрос 01.06.2022
+  // let randomQuest = randomiser();
 
+  // answerOne.textContent = questions[i].answers[randomQuest[0]];
+  // answerTwo.textContent = questions[i].answers[randomQuest[1]];
+  // answerThree.textContent = questions[i].answers[randomQuest[2]];
+  // for (let n = 0; n <= 2; n++) {
+  //   inputs[n].value = randomQuest[n] + 1;
+  // }
   answerOne.textContent = questions[i].answers[0];
   answerTwo.textContent = questions[i].answers[1];
   answerThree.textContent = questions[i].answers[2];
+
   document.querySelectorAll('.symb').forEach((symb) => symb.remove());
   // allSymbls.forEach(symbl => (symbl.style.opacity = 0));
   comentRule.textContent = questions[i].answerComment;
@@ -184,7 +171,6 @@ answerForm.addEventListener('submit', function (e) {
   for (let entry of data) {
     userAnswer = Number(entry[1]);
   }
-  //   console.log(userAnswer);
   e.preventDefault();
   // inputs.forEach(input => input.setAttribute('disabled', true));
   // checkBtn.setAttribute('disabled', true);
@@ -218,7 +204,6 @@ prewiousBtn.addEventListener('click', function (e) {
 });
 
 // Имплементация включения радио кнопок и кнопки проверки
-
 function nextQuestion() {
   starterTest(kotelnQuest, i);
   inputs.forEach((input) => {
@@ -230,7 +215,6 @@ function nextQuestion() {
 }
 
 // Имплементация навигации по вопросам в правом поле 30.05.2022
-
 allNumDivs.forEach((numDiv) =>
   numDiv.addEventListener('click', function (e) {
     // console.log(e);
@@ -242,7 +226,6 @@ allNumDivs.forEach((numDiv) =>
 );
 
 // Имплементация рандомного ответа
-
 function randomiser() {
   let randInd = [];
   let cycle = 0;
@@ -256,3 +239,4 @@ function randomiser() {
   }
   return randInd;
 }
+// randomiser();
